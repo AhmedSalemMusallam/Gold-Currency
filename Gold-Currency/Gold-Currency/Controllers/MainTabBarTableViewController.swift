e249d56a0bbacdac4a0d3de5c570ac5eb1f04101
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2022-12-22 22:04:39 +0200

    Setting initial App UI View

---
+++ Gold-Currency/Gold-Currency/Controllers/MainTabBarTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarTableViewController.swift
+//  Gold-Currency
+//
+//  Created by Ahmed Salem on 22/12/2022.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        
+        let vc1 = UINavigationController(rootViewController: MainViewController())
+        let vc2 = UINavigationController(rootViewController: SettingsViewController())
+        
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        
+        
+        vc1.title = "Home"
+        vc2.title = "Settings"
+        
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([vc1,vc2], animated: true)
+        
+    }
+}
