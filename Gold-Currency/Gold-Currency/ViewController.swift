0931bf3fcab8c3cda03afb095cbbd7f0cdfd88d7
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2022-12-22 21:45:10 +0200

    Creating Project Commit

---
+++ Gold-Currency/Gold-Currency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gold-Currency
+//
+//  Created by Ahmed Salem on 22/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
