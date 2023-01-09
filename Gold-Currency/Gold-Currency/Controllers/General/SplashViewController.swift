d7778b316e981bc4a97e26f2c446b016f6064c8e
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-01-09 22:50:26 +0200

    Splash Screen - Animation For Logo & App Name - Commit

---
+++ Gold-Currency/Gold-Currency/Controllers/General/SplashViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SplashViewController.swift
+//  Gold-Currency
+//
+//  Created by Ahmed Salem on 09/01/2023.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    @IBOutlet weak var logoImage: UIImageView!
+    
+    @IBOutlet weak var appName: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Fire Segue to OnBoarding Screen
+        PerformsSplashSegue()
+        //Aniamte Logo and App Name
+        animateLogoAndAppName()
+        
+        
+    }
+    
+    //Mark:- functions
+    private func PerformsSplashSegue()
+    {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            self.performSegue(withIdentifier: "PerformsSplashSegue", sender: nil)
+//            print("Timer fired!")
+        }
+    }
+    
+    private func animateLogoAndAppName()
+    {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.2) {
+            // Scaling
+            UIView.animate(withDuration: 0.4, animations: {
+                self.logoImage.transform = CGAffineTransform(scaleX: 2, y: 2)
+                self.appName.transform = CGAffineTransform(translationX: 0, y: 100)
+                
+                })
+            
+            
+        }
+        
+       
+        
+    }
+
+    
+
+}
